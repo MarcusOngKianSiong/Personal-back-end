d329f5b8817d395758f4a4af433136d645baa8a0
Marcus <marcusongkiansiong@gmail.com>
2023-12-12 11:54:18 +0800

    New functionality: added new route to retrieve data from notion simple table in this link: https://www.notion.so/Notion-fd0e8c50418f4360b512be4b5303d5cf?pvs=4

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors')
+require('dotenv').config();
+const {simpleTableManipulation,NotionOperations} = require('./custom-notion/interface.js')
 const app = express()
+const tableManagement = new simpleTableManipulation(process.env.project_table_id,{ table_width: process.env.table_width, has_column_header: process.env.has_column_header, has_row_header: process.env.has_row_header })
 
 
 app.use(cors({origin: '*'}))
 
 app.get('/',(req,res)=>{
     console.log("Running the operation....")
+
     res.send({
         
         projectName: {
@@ -16,6 +20,12 @@ app.get('/',(req,res)=>{
     })
 })
 
+
+app.get('/projects',async (req,res)=>{
+        // Have the table id ready
+        const data = await tableManagement.extractSimpleTableContents();
+        res.send(data);
+})
 
 app.listen(3001,()=>{
     console.log('server running....')
